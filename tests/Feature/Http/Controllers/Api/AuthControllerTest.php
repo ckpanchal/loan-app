9fc73ec41de0b3ff25c29b991d075b2505d90391
Chintan Panchal <chintan@maxput.com>
2022-02-17 16:59:51 +0530

    Added some tests for auth controller

--- tests/Feature/Http/Controllers/Api/AuthControllerTest.php
+++ tests/Feature/Http/Controllers/Api/AuthControllerTest.php
@@ -4,6 +4,8 @@ namespace Tests\Feature\Http\Controllers\Api;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use App\Models\User;
+use JWTAuth;
 use Tests\TestCase;
 
 class AuthControllerTest extends TestCase
@@ -55,6 +57,64 @@ class AuthControllerTest extends TestCase
         $response = $this->json('POST', route('login'), $formData);
         $response->assertStatus(401);
     }  
+
+    public function test_user_profile_api_works_successfully()
+    {
+        $this->withoutExceptionHandling();
+        $user = User::where(['email' => 'user@example.com'])->first(); // User
+        $token = JWTAuth::fromUser($user);
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->json('GET', route('profile'));
+        $response->assertStatus(200);
+        $decodeResponse = $response->decodeResponseJson();
+        $this->assertEquals(true, $decodeResponse['status']);
+    }
+
+    public function test_refresh_token_api_works_successfully()
+    {
+        $this->withoutExceptionHandling();
+        $user = User::where(['email' => 'user@example.com'])->first(); // User
+        $token = JWTAuth::fromUser($user);
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->json('POST', route('refresh'));
+        $response->assertStatus(200);
+        $decodeResponse = $response->decodeResponseJson();
+        $this->assertEquals(true, $decodeResponse['status']);
+    }
+
+    public function test_logout_api_works_successfully()
+    {
+        $this->withoutExceptionHandling();
+        $user = User::where(['email' => 'user@example.com'])->first(); // User
+        $token = JWTAuth::fromUser($user);
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->json('POST', route('logout'));
+        $response->assertStatus(200);
+        $decodeResponse = $response->decodeResponseJson();
+        $this->assertEquals(true, $decodeResponse['status']);
+    }
+
+    public function test_access_token_not_working_after_logout()
+    {
+        $this->withoutExceptionHandling();
+        $user = User::where(['email' => 'user@example.com'])->first(); // User
+        $token = JWTAuth::fromUser($user);
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->json('POST', route('logout'));
+        $response->assertStatus(200);
+
+        // Access profile api
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->json('GET', route('profile'));
+        $response->assertStatus(401);
+        $decodeResponse = $response->decodeResponseJson();
+        $this->assertEquals(false, $decodeResponse['status']);
+    }
 
     public function tearDown(): void
     {
